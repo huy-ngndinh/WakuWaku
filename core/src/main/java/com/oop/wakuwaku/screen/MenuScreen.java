560cfc6e57ce813419f993016d9ad00339148e67
Zoan-pUwUq <doanhvth@gmail.com>
2026-05-12 21:57:22 +0700

    update menu screen

--- core/src/main/java/com/oop/wakuwaku/screen/MenuScreen.java
+++ core/src/main/java/com/oop/wakuwaku/screen/MenuScreen.java
@@ -10,9 +10,11 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -25,9 +27,14 @@ import com.oop.wakuwaku.Main;
 public class MenuScreen extends ScreenAdapter {
     private Main game;
     private Stage stage;
-    private Texture bgTex, playTex, setTex;
-    private ImageButton play, sett;
+    private Texture bgTex, playTex, setTex, popupTex, closeTex;
+    private ImageButton play, sett, closeBtn;
     private FitViewport viewport;
+    private boolean showPopup = false;
+
+    private Slider musicSlider, effectSlider;
+    private Skin skin;
+
     private final float VIRTUAL_WIDTH = 1280;
     private final float VIRTUAL_HEIGHT = 720;
 
@@ -41,12 +48,21 @@ public class MenuScreen extends ScreenAdapter {
         stage = new Stage(viewport);
         Gdx.input.setInputProcessor(stage); 
 
+        skin = new Skin(Gdx.files.internal("cloud-form-ui.json"));
+
         bgTex = new Texture("background.jpg");
         playTex = new Texture("play.png");
         setTex  = new Texture("settings.png");
+        popupTex = new Texture("settings_panel.png");
+        closeTex = new Texture("close.png");
 
         play = new ImageButton(new TextureRegionDrawable(new TextureRegion(playTex)));
         sett = new ImageButton(new TextureRegionDrawable(new TextureRegion(setTex)));
+        closeBtn = new ImageButton(new TextureRegionDrawable(new TextureRegion(closeTex)));
+
+        musicSlider = new Slider(0, 1, 0.1f, false, skin);
+        musicSlider.setValue(0.5f);
+        musicSlider.setSize(300, 200);
 
         Table root = new Table();
         root.setFillParent(true);
@@ -54,16 +70,42 @@ public class MenuScreen extends ScreenAdapter {
 
         Table menuButtons = new Table();
         menuButtons.right().bottom(); 
-        menuButtons.add(play).size(410, 220).padBottom(-57);
+        menuButtons.add(play).size(410, 220).padLeft(1110).padBottom(-59);
         menuButtons.row();
-        menuButtons.add(sett).size(410, 210);
+        menuButtons.add(sett).size(410, 210).padLeft(1110);
+        menuButtons.add(closeBtn).size(235, 125).padLeft(-3).padBottom(-30);
 
         root.add(menuButtons).expand().right().bottom().padRight(-3).padBottom(-30);
 
         play.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new GameScreen(game)); 
+                    if(!showPopup)   game.setScreen(new GameScreen(game));
+                }
+        });
+
+        sett.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                showPopup = true;
+                System.out.println("Nut Settings da duoc bam!");
+            }
+        });
+
+        musicSlider.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                float vol = musicSlider.getValue();
+                // Cập nhật âm lượng thực tế
+                System.out.println("Music Volume: " + vol);
+            }
+        });
+
+        closeBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                showPopup = false;
+                System.out.println("Nut Close da duoc bam!");
             }
         });
     }
@@ -72,13 +114,27 @@ public class MenuScreen extends ScreenAdapter {
     public void render(float delta) {
         ScreenUtils.clear(Color.BLACK);
 
-        if (game.batch != null) {
-            game.batch.setProjectionMatrix(viewport.getCamera().combined);
-            game.batch.begin();
-            game.batch.draw(bgTex, 0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
-            game.batch.end();
+        game.batch.setProjectionMatrix(viewport.getCamera().combined);
+        game.batch.begin();
+        game.batch.draw(bgTex, 0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
+        if (showPopup) {
+            float pW = popupTex.getWidth() / 2f;
+            float pH = popupTex.getHeight() / 2f;
+            float pX = (VIRTUAL_WIDTH - pW) / 2f;
+            float pY = (VIRTUAL_HEIGHT - pH) / 2f;
+            musicSlider.setVisible(true);
+            game.batch.draw(popupTex, pX, pY, pW, pH);
+            musicSlider.setPosition(pX + 500, pY + 320);
+            closeBtn.setVisible(true);
+            closeBtn.setPosition(pX + 481, pY + 210);
+            stage.addActor(closeBtn);
+            stage.addActor(musicSlider);
         }
-
+        else {
+            musicSlider.setVisible(false);
+            closeBtn.setVisible(false);
+        }
+        game.batch.end();
         stage.act(delta);
         stage.draw();
     }
@@ -89,6 +145,7 @@ public class MenuScreen extends ScreenAdapter {
         bgTex.dispose();
         playTex.dispose();
         setTex.dispose();
+        popupTex.dispose();
     }
 
     @Override
