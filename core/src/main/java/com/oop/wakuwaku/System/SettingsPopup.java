d8faa9089e29219b3a86316f38834252840c11ad
Zoan-pUwUq <doanhvth@gmail.com>
2026-06-04 00:18:56 +0700

    update setting panel

--- core/src/main/java/com/oop/wakuwaku/System/SettingsPopup.java
+++ core/src/main/java/com/oop/wakuwaku/System/SettingsPopup.java
@@ -1,7 +1,6 @@
 package com.oop.wakuwaku.System;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -11,7 +10,6 @@ public class SettingsPopup {
     private final Main game;
     private final Texture popupTex;
     private final Texture catHeadTex;
-    private final Texture closeTex;
 
     public float musicVolume = 0.5f;
     public float effectVolume = 0.5f;
@@ -20,20 +18,15 @@ public class SettingsPopup {
     private final int sliderY_music = 400, sliderY_effect = 450;
     private final int catWidth = 72, catHeight = 65;
 
-    private final int closeX = 521, closeY = 235, closeW = 235, closeH = 125;
 
     private boolean isTouching_Music = false;
     private boolean isTouching_Effect = false;
-    private boolean isClickingClose = false; 
 
-    private final Sound clickSound;
 
     public SettingsPopup(Main game) {
         this.game = game;
         this.popupTex = new Texture("settings_panel.png");
         this.catHeadTex = new Texture("cat_head.png");
-        this.closeTex = new Texture("close.png");
-        this.clickSound = Gdx.audio.newSound(Gdx.files.internal("click.mp3"));
     }
 
     public void updateAndDraw(Viewport viewport) {
@@ -43,13 +36,7 @@ public class SettingsPopup {
             game.touchPos.set(Gdx.input.getX(), Gdx.input.getY());
             viewport.unproject(game.touchPos);
 
-            if (!isTouching_Music && !isTouching_Effect) {
-                if (game.touchPos.x >= closeX && game.touchPos.x <= closeX + closeW && game.touchPos.y >= closeY && game.touchPos.y <= closeY + closeH) {
-                    isClickingClose = true;
-                }
-            }
-
-            if (!isTouching_Effect && !isClickingClose) {
+            if (!isTouching_Effect) {
                 if (isTouching_Music) {
                     float x = MathUtils.clamp(game.touchPos.x, sliderMinX, sliderMaxX);
                     musicVolume = (x - sliderMinX) / (sliderMaxX - sliderMinX);
@@ -59,7 +46,7 @@ public class SettingsPopup {
                 }
             }
 
-            if (!isTouching_Music && !isClickingClose) {
+            if (!isTouching_Music) {
                 if (isTouching_Effect) {
                     float x = MathUtils.clamp(game.touchPos.x, sliderMinX, sliderMaxX);
                     effectVolume = (x - sliderMinX) / (sliderMaxX - sliderMinX);
@@ -70,11 +57,6 @@ public class SettingsPopup {
             }
         }
         else {
-            if (isClickingClose) {
-                clickSound.play(effectVolume);
-                game.showPopup = false;
-                isClickingClose = false;
-            }
             isTouching_Music = false;
             isTouching_Effect = false;
         }
@@ -83,7 +65,6 @@ public class SettingsPopup {
 
         game.batch.draw(popupTex, 340, 200, 600, 360);
 
-        game.batch.draw(closeTex, closeX, closeY, closeW, closeH);
 
         float musicCatX = sliderMinX + musicVolume * (sliderMaxX - sliderMinX) - catWidth / 2f;
         float musicCatY = sliderY_music - catHeight / 2f;
@@ -97,7 +78,5 @@ public class SettingsPopup {
     public void dispose() {
         popupTex.dispose();
         catHeadTex.dispose();
-        closeTex.dispose();
-        clickSound.dispose();
     }
 }
